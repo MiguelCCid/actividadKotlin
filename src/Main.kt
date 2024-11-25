c2c862c50ffc8664fa2dafe42a3d86507ba22376
miguelcc82 <miguelcc82@educastur.es>
2024-11-25 18:35:46 +0100

    segundo ejercicio

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,22 @@
+import java.util.*
 
 fun main() {
+    val palabras= Scanner(System.`in`)
+    val anagrama: Boolean
+    print("Introduzca una palabra: ")
+    val palabra1= palabras.nextLine()
+    print("Introduzca otra palabra: ")
+    val palabra2= palabras.nextLine()
+    anagrama=anagramas(palabra1,palabra2)
+    print("Las palabras $")
 
 
     }
+    fun anagramas(palabra1:String, palabra2: String): Boolean{
+        if (palabra1.length != palabra2.length){
+            return false
+        }
+        return palabra1.toCharArray().sorted() == palabra2.toCharArray().sorted()
+}
 
 

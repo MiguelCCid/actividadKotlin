9baef65c0d6402c777f543be3546d8d7715de586
Miguel Calzon <miguel.calzon@informatica.cifpaviles.pa>
2024-11-26 19:47:00 +0100

    ejercicio 4

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,59 @@
-import java.util.*
 
 fun main() {
-    val palabras= Scanner(System.`in`)
-    val anagrama: Boolean
-    print("Introduzca una palabra: ")
-    val palabra1= palabras.nextLine()
-    print("Introduzca otra palabra: ")
-    val palabra2= palabras.nextLine()
-    anagrama=anagramas(palabra1,palabra2)
-    print("Las palabras $palabra1 y $palabra2 son anagramas?: $anagrama")
+    val atleta:List<accion> = listOf(accion.correr, accion.saltar, accion.saltar, accion.correr)
+    val carrera:List<carrera> = listOf(carrera.suelo, carrera.valla, carrera.suelo, carrera.suelo)
 
+    println(dibujarCarrera(atleta, carrera) + " "+ completaCarrera(atleta,carrera))
+}
+enum class accion(val action:String){
+    correr("_"),
+    saltar("|")
+}
+
+enum class carrera(val momento:String){
+    suelo("suelo"),
+    valla("valla")
+}
+fun completaCarrera(atleta: List<accion>, carrera: List<carrera>): Boolean{
+    val longitudCarrera: Int = carrera.size
+    var obstaculosBien :Int = 0
+    if(atleta.size!=carrera.size){
+        return false
+    }
+    for (x:Int in 0 ..longitudCarrera-1){
+        if (atleta[x].action == "_" && carrera[x].momento == "suelo" || atleta[x].action == "|" && carrera[x].momento == "valla" ){
+            obstaculosBien++
+        }
 
     }
-    fun anagramas(palabra1:String, palabra2: String): Boolean{
-        if (palabra1.length != palabra2.length || palabra1==palabra2){
-            return false
-        }
-        return palabra1.toCharArray().sorted() == palabra2.toCharArray().sorted()
+    if (obstaculosBien==longitudCarrera){
+        return true
+    } else {
+        return false
+    }
+
 }
+
+fun dibujarCarrera(atleta: List<accion>, carrera: List<carrera>): String{
+    val longitudCarrera: Int = carrera.size
+    var carreraDibujada: String=""
+    for (x:Int in 0 ..longitudCarrera-1){
+       if(atleta[x].action == "_" && carrera[x].momento == "suelo"){
+           carreraDibujada=carreraDibujada+"_"
+       } else if(atleta[x].action == "|" && carrera[x].momento == "valla") {
+           carreraDibujada=carreraDibujada+"|"
+       } else if(atleta[x].action == "|" && carrera[x].momento == "suelo") {
+           carreraDibujada=carreraDibujada+"x"
+       } else if(atleta[x].action == "_" && carrera[x].momento == "valla") {
+           carreraDibujada=carreraDibujada+"/"
+       } else {
+           carreraDibujada=carreraDibujada+"?"
+       }
+
+    }
+    return carreraDibujada
+    }
+
+
 
 

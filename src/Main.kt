c44bcdc7be4c5ea9fa8fbbbe29a3c32f61d71f22
miguelcc82 <miguelcc82@educastur.es>
2024-11-27 17:49:35 +0100

    ejercicio 4 corregido

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,28 @@
-
 fun main() {
     val atleta:List<accion> = listOf(accion.correr, accion.saltar, accion.saltar, accion.correr)
-    val carrera:List<carrera> = listOf(carrera.suelo, carrera.valla, carrera.suelo, carrera.suelo)
+    val carrera:List<carrera> = listOf(carrera.suelo, carrera.valla, carrera.suelo, carrera.suelo, carrera.valla, carrera.valla, carrera.valla)
 
     println(dibujarCarrera(atleta, carrera) + " "+ completaCarrera(atleta,carrera))
 }
 enum class accion(val action:String){
-    correr("_"),
-    saltar("|")
+    correr("correr"),
+    saltar("saltar")
 }
 
 enum class carrera(val momento:String){
-    suelo("suelo"),
-    valla("valla")
+    suelo("_"),
+    valla("|")
 }
+
 fun completaCarrera(atleta: List<accion>, carrera: List<carrera>): Boolean{
     val longitudCarrera: Int = carrera.size
     var obstaculosBien :Int = 0
     if(atleta.size!=carrera.size){
         return false
     }
+
     for (x:Int in 0 ..longitudCarrera-1){
-        if (atleta[x].action == "_" && carrera[x].momento == "suelo" || atleta[x].action == "|" && carrera[x].momento == "valla" ){
+        if (atleta[x].action == "correr" && carrera[x].momento == "_" || atleta[x].action == "saltar" && carrera[x].momento == "|" ){
             obstaculosBien++
         }
 
@@ -35,21 +36,31 @@ fun completaCarrera(atleta: List<accion>, carrera: List<carrera>): Boolean{
 }
 
 fun dibujarCarrera(atleta: List<accion>, carrera: List<carrera>): String{
-    val longitudCarrera: Int = carrera.size
+    val longitudCarrera: Int = carrera.size -1
+    var pasosExtra: Int = if (atleta.size > carrera.size) {
+        atleta.size - carrera.size
+    } else {
+        carrera.size - atleta.size
+    }
     var carreraDibujada: String=""
-    for (x:Int in 0 ..longitudCarrera-1){
-       if(atleta[x].action == "_" && carrera[x].momento == "suelo"){
+    for (x:Int in 0 ..longitudCarrera - pasosExtra){
+       if(atleta[x].action == "correr" && carrera[x].momento == "_"){
            carreraDibujada=carreraDibujada+"_"
-       } else if(atleta[x].action == "|" && carrera[x].momento == "valla") {
+       } else if(atleta[x].action == "saltar" && carrera[x].momento == "|") {
            carreraDibujada=carreraDibujada+"|"
-       } else if(atleta[x].action == "|" && carrera[x].momento == "suelo") {
+       } else if(atleta[x].action == "saltar" && carrera[x].momento == "_") {
            carreraDibujada=carreraDibujada+"x"
-       } else if(atleta[x].action == "_" && carrera[x].momento == "valla") {
+       } else if(atleta[x].action == "correr" && carrera[x].momento == "|") {
            carreraDibujada=carreraDibujada+"/"
        } else {
            carreraDibujada=carreraDibujada+"?"
        }
 
+    }
+
+    while (pasosExtra>0){
+        carreraDibujada=carreraDibujada+"?"
+        pasosExtra--
     }
     return carreraDibujada
     }
